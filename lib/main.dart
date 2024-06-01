0383a4519f7ad28a5821fb95df976f994a49e9d8
noniyaza <rajabeliud45@gmail.com>
2024-06-01 23:30:28 +0300

    Revert "fake commite"
    This reverts commit db230a204513bae90a2219005613fe565b3321ed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:news_and_forums/firebase_options.dart';
 import 'package:news_and_forums/screens/login.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
 
--- lib/screens/admin_home.dart
+++ lib/screens/admin_home.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:news_and_forums/screens/admin_news.dart';
-import 'package:news_and_forums/screens/login.dart';
 
 class AdminHome extends StatefulWidget {
   const AdminHome({super.key});
@@ -15,112 +13,57 @@ class _AdminHomeState extends State<AdminHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Colors.lightGreen),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 80.0, left: 22.0),
-              child: Text(
-                "ATC \nNews & Forum",
-                style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.white70,
-                    fontWeight: FontWeight.bold),
-              ),
+        body: Stack(
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(color: Colors.lightGreen),
+          child: const Padding(
+            padding: EdgeInsets.only(top: 80.0, left: 22.0),
+            child: Text(
+              "ATC \nNews & Forum",
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white70,
+                  fontWeight: FontWeight.bold),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 200.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-                color: Colors.white70,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 200.0),
+          child: Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
               ),
-              height: double.infinity,
-              width: double.infinity,
-              child: Padding(
-                padding: EdgeInsets.only(top: 40, left: 28.0, right: 28.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => AdminNews()));
-                                },
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Card(
-                                      child: Container(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 20, vertical: 10),
-                                          height: 120,
-                                          // width: MediaQuery.of(context).size.width,
-                                          width: 130,
-                                          child: Image.asset('image/top.webp')),
-                                    ),
-                                    const Text(
-                                      'News',
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              Column(
-                                children: [
-                                  Card(
-                                    child: Container(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 20, vertical: 10),
-                                        height: 120,
-                                        width: 130,
-                                        child: Image.asset('image/top.webp')),
-                                  ),
-                                  const Text(
-                                    'Forums',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Column(
+              color: Colors.white70,
+            ),
+            height: double.infinity,
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.only(top: 40, left: 28.0, right: 28.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => AdminNews()));
+                              },
+                              child: Column(
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
                                   Card(
@@ -130,60 +73,103 @@ class _AdminHomeState extends State<AdminHome> {
                                         height: 120,
                                         // width: MediaQuery.of(context).size.width,
                                         width: 130,
-                                        child:
-                                            Image.asset('image/request.png')),
+                                        child: Image.asset('image/top.webp')),
                                   ),
                                   const Text(
-                                    'Admin',
+                                    'News',
                                     style: TextStyle(
                                         fontSize: 18,
                                         fontWeight: FontWeight.bold),
                                   )
                                 ],
                               ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              Column(
-                                children: [
-                                  Card(
-                                    child: Container(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 20, vertical: 10),
-                                        height: 120,
-                                        width: 130,
-                                        child: Image.asset('image/forum2.jpg')),
-                                  ),
-                                  const Text(
-                                    'Other',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Column(
+                              children: [
+                                Card(
+                                  child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20, vertical: 10),
+                                      height: 120,
+                                      width: 130,
+                                      child: Image.asset('image/top.webp')),
+                                ),
+                                const Text(
+                                  'Forums',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Card(
+                                  child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20, vertical: 10),
+                                      height: 120,
+                                      // width: MediaQuery.of(context).size.width,
+                                      width: 130,
+                                      child: Image.asset('image/request.png')),
+                                ),
+                                const Text(
+                                  'Admin',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Column(
+                              children: [
+                                Card(
+                                  child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20, vertical: 10),
+                                      height: 120,
+                                      width: 130,
+                                      child: Image.asset('image/forum2.jpg')),
+                                ),
+                                const Text(
+                                  'Other',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          FirebaseAuth.instance.signOut().then((value) {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => LoginPage()));
-          });
-        },
-        label: Text("Logout"),
-        icon: Icon(Icons.logout_outlined),
-      ),
-    );
+          ),
+        )
+      ],
+    ));
   }
 }
